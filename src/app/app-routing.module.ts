3acc58edd2a54d5aa6a6581e2b1f84433059f665
Gabriel Nascimento <gabriel.mfn@gmail.com>
2020-08-13 17:08:56 -0300

    Removida a pagina de suporte

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,10 +13,6 @@ const routes: Routes = [
     loadChildren: () => import('./pages/account/account.module').then(m => m.AccountModule)
   },
   {
-    path: 'support',
-    loadChildren: () => import('./pages/support/support.module').then(m => m.SupportModule)
-  },
-  {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
   },
