6d9cbb85000b8487fd052ddcaf42d3117b85e46b
Gabriel Nascimento <gabriel.mfn@gmail.com>
2020-08-14 19:53:51 -0300

    Criada variaveis de ambiente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,20 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
-  production: false
+  production: false,
+
+  // VARIÁVEIS ESSENCIAIS PARA A APLICAÇÃO
+  baseUrl: 'http://localhost:8000',
+  
+  /*
+  * VARIÁVEIS DA API DE AUTENTICAÇÃO PASSPORT
+  * Todas as configurações devem estar de acordo com o cliente gerado pela API.
+  * Se não souber suas configurações, consulte o banco de dados
+  */
+  grant_type: "password",
+  client_id: 2,
+  client_secret: "EwHm4eEmlWpFuDbBvxpjQWS9LlQgCsJbLZRRKWUG",
+  scope: ""
 };
 
 /*
