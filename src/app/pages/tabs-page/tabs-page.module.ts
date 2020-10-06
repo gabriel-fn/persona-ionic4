93669386dd486ba7572dd2412ef2dbc766d7fc71
Gabriel Nascimento <gabriel.mfn@gmail.com>
2020-10-06 16:48:53 -0300

    Remoção do import do modulo about no modulo tabs

--- src/app/pages/tabs-page/tabs-page.module.ts
+++ src/app/pages/tabs-page/tabs-page.module.ts
@@ -5,7 +5,6 @@ import { IonicModule } from '@ionic/angular';
 import { TabsPage } from './tabs-page';
 import { TabsPageRoutingModule } from './tabs-page-routing.module';
 
-import { AboutModule } from '../about/about.module';
 import { ScheduleModule } from '../schedule/schedule.module';
 import { SessionDetailModule } from '../session-detail/session-detail.module';
 import { SpeakerDetailModule } from '../speaker-detail/speaker-detail.module';
@@ -13,7 +12,6 @@ import { SpeakerListModule } from '../speaker-list/speaker-list.module';
 
 @NgModule({
   imports: [
-    AboutModule,
     CommonModule,
     IonicModule,
     ScheduleModule,
